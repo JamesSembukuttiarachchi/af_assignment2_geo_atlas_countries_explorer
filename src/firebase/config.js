21f8ceb0ff7aa0dadcb5ced493039ee574ccd76a
James Sembukuttiarachchi <jamessanjayasem@gmail.com>
2025-05-03 02:04:37 +0530

    Refactor Firebase configuration to use environment variables

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,23 +5,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // Replace with your actual Firebase config values
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-//   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-//   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-//   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyCoYniz7-nk937xaswIG0CyyK1rfwEOTlY",
-  authDomain: "countries-explorer-38b02.firebaseapp.com",
-  projectId: "countries-explorer-38b02",
-  storageBucket: "countries-explorer-38b02.firebasestorage.app",
-  messagingSenderId: "268864733775",
-  appId: "1:268864733775:web:9930199a85b91d75a1e03a",
-  measurementId: "G-YKWNSGFJQJ",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
